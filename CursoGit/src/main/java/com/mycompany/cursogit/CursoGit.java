fb7eecacd32b5bcdab230f7e40178f42e22ee768
regiiniita <reginamenesesj09@gmail.com>
2026-02-12 17:03:12 -0700

    feat: commit de prueba taller
    Este commit se realizó para subir el proyecto a GitHub

---
+++ CursoGit/src/main/java/com/mycompany/cursogit/CursoGit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cursogit;
+
+/**
+ *
+ * @author Programación Interme
+ */
+public class CursoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hecho por Reginita");
+    }
+}
